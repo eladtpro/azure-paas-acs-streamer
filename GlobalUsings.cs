c324f173023e9ef0f863e91fb11e3d66f2f6b52e
eladtpro <eladt.pro@gmail.com>
2022-11-29 16:56:32 +0200

    html player

---
+++ GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.IO;
+global using System.Threading.Tasks;
+global using Azure.Storage.Blobs;
+global using Azure.Storage.Blobs.Models;
+global using Microsoft.Extensions.Logging;
+
+global using Microsoft.Azure.WebJobs;
